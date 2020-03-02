325596fd8231537c55d13061d1d1b144761601e7
Moritz W <mwi4339@students.fhv.at>
2020-03-02 12:08:50 +0100

    Make sure to evaluate operators in right order

--- src/classifier.py
+++ src/classifier.py
@@ -19,18 +19,18 @@ class Classifier:
         channel = self.classes[name]
 
         for rgbcolor in rgbcolors:
-            channel['r'][rgbcolor[0] // 64] = channel['r'][rgbcolor[0] // 64] | (1 << (64 - rgbcolor[0] % 64))
-            channel['g'][rgbcolor[1] // 64] = channel['g'][rgbcolor[1] // 64] | (1 << (64 - rgbcolor[1] % 64))
-            channel['b'][rgbcolor[2] // 64] = channel['b'][rgbcolor[2] // 64] | (1 << (64 - rgbcolor[2] % 64))
+            channel['r'][rgbcolor[0] // 64] = channel['r'][rgbcolor[0] // 64] | (1 << (64 - (rgbcolor[0] % 64)))
+            channel['g'][rgbcolor[1] // 64] = channel['g'][rgbcolor[1] // 64] | (1 << (64 - (rgbcolor[1] % 64)))
+            channel['b'][rgbcolor[2] // 64] = channel['b'][rgbcolor[2] // 64] | (1 << (64 - (rgbcolor[2] % 64)))
 
 
     def classify (self, rgbcolor) -> str:
         _class = "unknown"
         for classname, colorclass in self.classes.items():
             inclass = \
-                bool(colorclass['r'][rgbcolor[0] // 64] >> (64 - rgbcolor[0] % 64) & 1) and \
-                bool(colorclass['g'][rgbcolor[1] // 64] >> (64 - rgbcolor[1] % 64) & 1) and \
-                bool(colorclass['b'][rgbcolor[2] // 64] >> (64 - rgbcolor[2] % 64) & 1)
+                bool(colorclass['r'][rgbcolor[0] // 64] >> (64 - (rgbcolor[0] % 64)) & 1) and \
+                bool(colorclass['g'][rgbcolor[1] // 64] >> (64 - (rgbcolor[1] % 64)) & 1) and \
+                bool(colorclass['b'][rgbcolor[2] // 64] >> (64 - (rgbcolor[2] % 64)) & 1)
 
             if inclass:
                 _class = classname
