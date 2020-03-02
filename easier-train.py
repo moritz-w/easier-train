3e434c8d36cea886da740ef8444f08a77a0b96f7
Moritz W <mwi4339@students.fhv.at>
2020-03-02 22:30:07 +0100

    Update test code

--- easier-train.py
+++ easier-train.py
@@ -2,17 +2,23 @@ import sys
 sys.path.append("./src")
 
 from detector import Detector
-from classifier import Classifier
+from classifier import SingleColorClassifier, MultiColorClassifier
 
 
 if __name__ == '__main__':
-    d = Detector("resource/img/testimage2.png")
-    c = Classifier()
+    detector = Detector("resource/img/testimage2.png")
+    classifier = MultiColorClassifier()
     # d.scan(71, 67)
-    d.scan (97, 97)
-    print (d.summary)
-    c.classifyAs(d.colors, "blue")
-    print (c.classify([33, 36, 114]))
-    print(c.classify([33, 36, 0]))
+    detector.scan (97, 97)
+    print (detector.summary)
 
-    d.save("resource/img/out.png")
+    classifier.classifyAs(detector.colors, "blue")
+
+    for color in detector.colors:
+        color = color.tolist()
+        cname = classifier.classify(color)
+        assert (cname == "blue")
+
+    classifier.writeOutput("resource/colorclasses/class1")
+
+    detector.save("resource/img/out.png")
