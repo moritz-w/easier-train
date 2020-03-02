2e9026859afad712a33caac4c63bbc134766ed77
Moritz W <mwi4339@students.fhv.at>
2020-03-02 22:29:56 +0100

    Add a MultiColorClassifier and methods to persist color classification

--- src/classifier.py
+++ src/classifier.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+from formatter import Formatter, JSONFormatter
 
-class Classifier:
+
+class SingleColorClassifier:
 
 
     def __init__(self):
@@ -41,3 +43,61 @@ class Classifier:
                 break
 
         return _class
+
+
+    def writeOutput (self, filename, formatter: Formatter = JSONFormatter()):
+        success = formatter.formatToFile(self.classes, filename)
+        if not success:
+            print (f"Failed to write output to '{filename}' with formatter '{formatter.name}'")
+        return success
+
+
+
+class MultiColorClassifier:
+
+    def __init__(self):
+        self.classes = {
+            "r": [0 for i in range(0, 256)],
+            "g": [0 for i in range(0, 256)],
+            "b": [0 for i in range(0, 256)],
+            "colormasks": {},
+            "colormask-reverse-lookup": {}
+        }
+        self.colormask = 1
+
+
+    def classifyAs (self, rgbcolors, name: str):
+
+        if isinstance(rgbcolors, np.ndarray):
+            rgbcolors = rgbcolors.tolist()
+
+        if name not in self.classes['colormasks']:
+            self.classes['colormasks'][name] = self.colormask
+            self.classes['colormask-reverse-lookup'][self.colormask] = name
+            self.colormask <<= 1
+
+        colormask = self.classes['colormasks'][name]
+
+        for rgbcolor in rgbcolors:
+            self.classes['r'][rgbcolor[0]] |= colormask
+            self.classes['g'][rgbcolor[1]] |= colormask
+            self.classes['b'][rgbcolor[2]] |= colormask
+
+
+    def classify (self, rgbcolor):
+
+        colorclass = self.classes['r'][rgbcolor[0]] & \
+                     self.classes['g'][rgbcolor[1]] & \
+                     self.classes['b'][rgbcolor[2]]
+
+        if colorclass == 0:
+            return None
+
+        return self.classes['colormask-reverse-lookup'].get(colorclass)
+
+
+    def writeOutput (self, filename, formatter: Formatter = JSONFormatter()):
+        success = formatter.formatToFile(self.classes, filename)
+        if not success:
+            print (f"Failed to write output to '{filename}' with formatter '{formatter.name}'")
+        return success
