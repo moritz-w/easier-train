4b13f68d1aed35f91990b2a3bc969503a613b758
Moritz W <mwi4339@students.fhv.at>
2020-03-02 12:01:25 +0100

    Use bit shifting for classifying a given RGB color

--- src/classifier.py
+++ src/classifier.py
@@ -16,22 +16,21 @@ class Classifier:
                 'b': [0, 0, 0, 0]
             }
 
-        r_channel = self.classes[name]['r']
-        g_channel = self.classes[name]['g']
-        b_channel = self.classes[name]['b']
+        channel = self.classes[name]
+
         for rgbcolor in rgbcolors:
-            r_channel[rgbcolor[0] // 64] = r_channel[rgbcolor[0] // 64] | (1 << (rgbcolor[0] % 64))
-            g_channel[rgbcolor[1] // 64] = g_channel[rgbcolor[1] // 64] | (1 << (rgbcolor[1] % 64))
-            b_channel[rgbcolor[2] // 64] = b_channel[rgbcolor[2] // 64] | (1 << (rgbcolor[2] % 64))
+            channel['r'][rgbcolor[0] // 64] = channel['r'][rgbcolor[0] // 64] | (1 << (64 - rgbcolor[0] % 64))
+            channel['g'][rgbcolor[1] // 64] = channel['g'][rgbcolor[1] // 64] | (1 << (64 - rgbcolor[1] % 64))
+            channel['b'][rgbcolor[2] // 64] = channel['b'][rgbcolor[2] // 64] | (1 << (64 - rgbcolor[2] % 64))
 
 
     def classify (self, rgbcolor) -> str:
         _class = "unknown"
         for classname, colorclass in self.classes.items():
-            inclass = bool(
-                int(colorclass['r'][rgbcolor[0]]) &
-                int(colorclass['g'][rgbcolor[1]]) &
-                int(colorclass['b'][rgbcolor[2]]))
+            inclass = \
+                bool(colorclass['r'][rgbcolor[0] // 64] >> (64 - rgbcolor[0] % 64) & 1) and \
+                bool(colorclass['g'][rgbcolor[1] // 64] >> (64 - rgbcolor[1] % 64) & 1) and \
+                bool(colorclass['b'][rgbcolor[2] // 64] >> (64 - rgbcolor[2] % 64) & 1)
 
             if inclass:
                 _class = classname
